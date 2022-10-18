500c16cace90219de179321238147c445613e14b
Mtuart <96501232+Mtuart@users.noreply.github.com>
2022-10-18 19:23:29 +0200

    Insert old login

--- GüntherExpress/login.php
+++ GüntherExpress/login.php
@@ -2,18 +2,70 @@
 include_once 'header.php';
 ?>
 
+<?php
+if (isset($_POST['login_button'])) {
+    // print_r($_POST);
+    $url = "https://www.google.com/recaptcha/api/siteverify";
+    $data = [
+        'secret' => "6Lcmco8iAAAAANGTI5fDqsmRWZhQrV5gBtHJMMvK",
+        'response' => $_POST['token'],
+        // 'remoteip' => $_SERVER['REMOTE_ADDR']
+    ];
+
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($data)
+        )
+    );
+
+
+    $context  = stream_context_create($options);
+    $response = file_get_contents($url, false, $context);
+
+    $res = json_decode($response, true);
+    if ($res['success'] == true) {
+        // Perform you logic here for ex:- save you data to database
+        echo '<div class="alert alert-success">
+			  		<strong>Success!</strong> Your inquiry successfully submitted.
+		 		  </div>';
+    } else {
+        echo '<div class="alert alert-warning">
+					  <strong>Error!</strong> You are not a human.
+				  </div>';
+    }
+}
+
+?>
+
+<html>
+
+<head>
+    <script src="https://www.google.com/recaptcha/api.js?render=6Lcmco8iAAAAAHr24hEwYoWG5PNRRFNWsgzT_TzR"></script>
+    <script>
+        function onSubmit(token) {
+            document.getElementById("login_form").submit();
+        }
+    </script>
+</head>
+
+</html>
+
 <section>
     <h2>Log-In</h2>
-    <form action="includes/login_include.php" method="post" id="login_form">
+    <form action="#" method="post" id="login_form">
         <input type="text" name="username" id="email/username" placeholder="E-mail/Benutzername" required><br>
         <input type="password" name="password" id="password" placeholder="Passwort" required><br>
 
-        <div class="recaptcha-holder"></div>
+        <button type="submit" value="Post" name="login_button" class="btn btn-success btn-block">Log In</button><br>
 
-        <button type="submit" name="login_button" >Log In</button><br>
+        <input type="hidden" id="token" name="token">
+
     </form>
 
 </section>
+
 <?php
 $errorMSG;
 if (isset($_GET["error"])) {
@@ -27,37 +79,15 @@ if (isset($_GET["error"])) {
 }
 ?>
 
+<script>
+    grecaptcha.ready(function() {
+        grecaptcha.execute('6Lcmco8iAAAAAHr24hEwYoWG5PNRRFNWsgzT_TzR').then(function(token) {
+            // console.log(token);
+            document.getElementById("token").value = token;
+        });
+    });
+</script>
 
 <?php
 include_once 'footer.php';
 ?>
-
-<script type="text/javascript">
-    var renderGoogleInvisibleRecaptcha = function() {
-        var form = document.forms[i];
-        var holder = form.querySelector('.recaptcha-holder');
-        if (null === holder) {
-            continue;
-        }
-
-        (function(frm) {
-
-            var holderId = grecaptcha.render(holder, {
-                'sitekey': '6Le4uYwiAAAAAMMmF6x_tZnrXtVenZngSYOalfKB',
-                'size': 'invisible',
-                'badge': 'bottomright', // possible values: bottomright, bottomleft, inline
-                'callback': function(recaptchaToken) {
-                    HTMLFormElement.prototype.submit.call(frm);
-                }
-            });
-
-            frm.onsubmit = function(evt) {
-                evt.preventDefault();
-                grecaptcha.execute(holderId);
-            };
-
-        })(form);
-    };
-</script>
-
-<script src="https://www.google.com/recaptcha/api.js?onload=renderGoogleInvisibleRecaptcha&render=explicit" async defer></script>
