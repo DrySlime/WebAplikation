1a5634b2ffdf71da59253efa1f57fed4f8fc4746
Mtuart <stui.eichler02@gmail.com>
2022-10-20 22:10:18 +0200

    Merge branch 'main' of https://github.com/DrySlime/WebAplikation

--- GüntherExpress/index.php
+++ GüntherExpress/index.php
@@ -2,12 +2,32 @@
 include_once 'header.php';
 ?>
 <?php
-    if (isset($_SESSION["useruid"])){
-        echo "<p<Moin ". $_SESSION["useruid"]."</p>";
-        
-        }else{
-        
-    }
+if (isset($_SESSION["useruid"])) {
+    echo "<p>Moin " . $_SESSION["useruid"] . "</p>";
+} else {
+}
+?>
+<?php
+require_once 'includes/dbh_include.php';
+require_once 'includes/functions_include.php';
+
+
+
+
+showExamples($conn, 2, "Schuhe");
+
+
+
+
+// for($i=1;$i<count($itemNames); $i++){
+//     try{
+//         @print $itemNames[$i];
+//     }catch(Exception $e){
+
+//     }
+
+// }
+
 ?>
 
     
