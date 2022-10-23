2f996622f124854b6ea5777f80da690394ef3971
Blobie1012 <nilsstawowy@gmail.com>
2022-10-23 03:05:18 +0200

    product.php

--- GüntherExpress/includes/functions_include.php
+++ GüntherExpress/includes/functions_include.php
@@ -201,27 +201,7 @@ function showCategoryList($conn){
     echo '</ul></div>';
 
 }
-//URl-Parameter werden ausgelesen
-function getURLParameter(){
-    $url =  "//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
-    $url_components = parse_url($url);
-    parse_str($url_components['query'], $params);
-    return $params;
-}
 
-//Produktdaten werden aus der Datenbank aufgerufen
-function getProductData($conn, $productID){
-
-    $sql = "SELECT * FROM Artikel WHERE ArtikelID = ?;";
-    $stmt = mysqli_stmt_init($conn);
-
-    mysqli_stmt_prepare($stmt,$sql);
-    mysqli_stmt_bind_param($stmt,"s",$productID,);
-    mysqli_stmt_execute($stmt);
-
-    $resultData = mysqli_stmt_get_result($stmt);
-    return mysqli_fetch_assoc($resultData);
-}
 function showExamples($conn,$amount,$category){
 
     //Gibt viele Attribute aus der Datenbank in einer ...
---
+++ GüntherExpress/includes/product_include.php
@@ -0,0 +1,32 @@
+
+
+
+
+
+<?php
+//URl-Parameter werden ausgelesen
+function getURLParameter(){
+    $url =  "//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+    $url_components = parse_url($url);
+    parse_str($url_components['query'], $params);
+    return $params;
+}
+
+//Produktdaten werden aus der Datenbank aufgerufen
+function getProductData($conn, $productID){
+
+    $sql = "SELECT * FROM product WHERE id = ?;";
+    $stmt = mysqli_stmt_init($conn);
+
+    mysqli_stmt_prepare($stmt,$sql);
+    mysqli_stmt_bind_param($stmt,"s",$productID,);
+    mysqli_stmt_execute($stmt);
+
+    $resultData = mysqli_stmt_get_result($stmt);
+    return mysqli_fetch_assoc($resultData);
+}
+
+function insert_into_shopping_card(){
+}
+
+?>
