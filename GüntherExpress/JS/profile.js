1e5c6c07e4c12011e3659ff4bbf3c90c6e1fbff1
SherwoodMan <dbhandydb@gmail.com>
2022-10-26 15:41:41 +0200

    php works

--- GüntherExpress/JS/profile.js
+++ GüntherExpress/JS/profile.js
@@ -2,10 +2,10 @@ var showProfile = 0;
 function showProfile() {
     alert('hier');
     if (showProfile == 0) {
-        document.getElementsByClassName('profile_icon').style.display = 'inline';
+        document.getElementsByClassName('profile_info').style.display = 'inline';
         showProfile = 1;
     } else {
-        document.getElementsByClassName('profile_icon').style.display = 'none';
+        document.getElementsByClassName('profile_info').style.display = 'none';
         showProfile = 0;
     }
     alert(showProfile);
