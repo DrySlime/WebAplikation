12f16cbe2129d3064b24cd39464cf9a51c5f3e78
DrySlime <chris.seibel.2000@gmail.com>
2022-11-19 19:16:19 +0100

    animation geschwindigkeit angepasst

--- GüntherExpress/JS/account.js
+++ GüntherExpress/JS/account.js
@@ -3,8 +3,8 @@ window.onscroll = function(event) {
     var scrollPos = doc.scrollTop;
 
     console.log(scrollPos);
-    document.getElementById("tmp123").style.top = -2*scrollPos+"px";
-    if(scrollPos>350){
-        doc.scrollTop=350;
+    document.getElementById("tmp123").style.top = -1.5*scrollPos+"px";
+    if(scrollPos>423){
+        doc.scrollTop=423;
     }
 }
