68d55a79016eb40251468f399a07f6bbbdeeb377
DrySlime <chris.seibel.2000@gmail.com>
2022-11-24 19:19:42 +0100

    Bestellungen funktionieren

--- GüntherExpress/includes/admin_functions_inc.php
+++ GüntherExpress/includes/admin_functions_inc.php
@@ -29,33 +29,213 @@ function getAllOrderedProducts($conn)
 
     return $allItems;
 }
-//function getAllOrderedProducts($conn)
-//{
-//    $sql = "SELECT * FROM shop_order ORDER BY order_date DESC ;";
-//    $stmt = mysqli_stmt_init($conn);
-//
-//    if (!mysqli_stmt_prepare($stmt, $sql)) {
-//        header("location: ../index.php?error=stmtfailed");
-//        exit();
-//    }
-////    mysqli_stmt_prepare($stmt, $sql);
-////    mysqli_stmt_bind_param($stmt, "s", $amount);
-//    mysqli_stmt_execute($stmt);
-//    $resultData = mysqli_stmt_get_result($stmt);
-//
-//    while ($row = mysqli_fetch_assoc($resultData)) {
-//        $array["order_id"] = $row["id"];
-//        $array["siteuser_id"] = $row["siteuser_id"];
-//        $array["order_date"] = $row["order_date"];
-//        $array["payment_method_id"] = $row["payment_method_id"];
-//        $array["shipping_address_id"] = $row["shipping_address_id"];
-//        $array["shipping_method_id"] = $row["shipping_method_id"];
-//        $array["order_total"] = $row["order_total"];
-//        $array["order_status_id"] = $row["order_status_id"];
-//
-//        $allItems[] = $array;
-//        unset($array);
-//    }
-//
-//    return $allItems;
-//}
+function getAllOrderedProductsFromUserID($conn, $userid)
+{
+    $allItems=null;
+    $sql = "SELECT * FROM shop_order WHERE siteuser_id=? ORDER BY order_date DESC ;";
+    $stmt = mysqli_stmt_init($conn);
+
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: ../index.php?error=stmtfailed");
+        exit();
+    }
+    mysqli_stmt_prepare($stmt, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $userid);
+    mysqli_stmt_execute($stmt);
+    $resultData = mysqli_stmt_get_result($stmt);
+
+    while ($row = mysqli_fetch_assoc($resultData)) {
+        $array["order_id"] = $row["id"];
+        $array["siteuser_id"] = $row["siteuser_id"];
+        $array["order_date"] = $row["order_date"];
+        $array["payment_method_id"] = $row["payment_method_id"];
+        $array["shipping_address_id"] = $row["shipping_address_id"];
+        $array["shipping_method_id"] = $row["shipping_method_id"];
+        $array["order_total"] = $row["order_total"];
+        $array["order_status_id"] = $row["order_status_id"];
+
+        $allItems[] = $array;
+        unset($array);
+
+    }
+
+    return $allItems;
+
+}
+function getUsername($conn,$userID){
+    $sql = "SELECT user_uid FROM site_user WHERE id=? ;";
+    $stmt = mysqli_stmt_init($conn);
+
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: ../index.php?error=stmtfailed");
+        exit();
+    }
+    mysqli_stmt_prepare($stmt, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $userID);
+    mysqli_stmt_execute($stmt);
+    $resultData = mysqli_stmt_get_result($stmt);
+
+    $row = mysqli_fetch_assoc($resultData);
+    $username = $row["user_uid"];
+
+
+
+    return $username;
+}
+function getPaymentMethod($conn,$paymentmethodID){
+    $sql = "SELECT value FROM payment_type WHERE id=? ;";
+    $stmt = mysqli_stmt_init($conn);
+
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: ../index.php?error=stmtfailed");
+        exit();
+    }
+    mysqli_stmt_prepare($stmt, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $paymentmethodID);
+    mysqli_stmt_execute($stmt);
+    $resultData = mysqli_stmt_get_result($stmt);
+
+    $row = mysqli_fetch_assoc($resultData);
+    $paymentmethod = $row["value"];
+
+
+
+    return $paymentmethod;
+}
+function getShippingAdress($conn,$id){
+    $sql = "SELECT * FROM address WHERE id=? ;";
+    $stmt = mysqli_stmt_init($conn);
+
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: ../index.php?error=stmtfailed");
+        exit();
+    }
+    mysqli_stmt_prepare($stmt, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $id);
+    mysqli_stmt_execute($stmt);
+    $resultData = mysqli_stmt_get_result($stmt);
+
+    $row = mysqli_fetch_assoc($resultData);
+    $shippingadress = $row["address_line1"]." ".$row["street_number"].", ".$row["postal_code"].", ".$row["city"];
+
+
+
+    return $shippingadress;
+}
+function getShippingMethod($conn,$id){
+    $sql = "SELECT shipping_name FROM shipping_method WHERE id=? ;";
+    $stmt = mysqli_stmt_init($conn);
+
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: ../index.php?error=stmtfailed");
+        exit();
+    }
+    mysqli_stmt_prepare($stmt, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $id);
+    mysqli_stmt_execute($stmt);
+    $resultData = mysqli_stmt_get_result($stmt);
+
+    $row = mysqli_fetch_assoc($resultData);
+    $shippingname = $row["shipping_name"];
+
+
+
+    return $shippingname;
+}
+function getOrderStatus($conn,$id){
+    $sql = "SELECT status FROM order_status WHERE id=? ;";
+    $stmt = mysqli_stmt_init($conn);
+
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: ../index.php?error=stmtfailed");
+        exit();
+    }
+    mysqli_stmt_prepare($stmt, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $id);
+    mysqli_stmt_execute($stmt);
+    $resultData = mysqli_stmt_get_result($stmt);
+
+    $row = mysqli_fetch_assoc($resultData);
+    $shippingname = $row["status"];
+
+
+
+    return $shippingname;
+}
+function getOrderLine($conn,$id){
+    $sql = "SELECT product_item_id, qty, price FROM order_line WHERE order_id=? ;";
+    $stmt = mysqli_stmt_init($conn);
+
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: ../index.php?error=stmtfailed");
+        exit();
+    }
+    mysqli_stmt_prepare($stmt, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $id);
+    mysqli_stmt_execute($stmt);
+    $resultData = mysqli_stmt_get_result($stmt);
+
+    while($row = mysqli_fetch_assoc($resultData)){
+        $product["product_item_id"] = $row["product_item_id"];
+        $product["qty"] = $row["qty"];
+        $product["price"] = $row["price"];
+        $productArr[]=$product;
+        unset($product);
+
+    }
+
+
+    return $productArr;
+}
+function getProductName($conn,$id){
+    $sql = "SELECT product_name FROM product WHERE id=? ;";
+    $stmt = mysqli_stmt_init($conn);
+
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: ../index.php?error=stmtfailed");
+        exit();
+    }
+    mysqli_stmt_prepare($stmt, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $id);
+    mysqli_stmt_execute($stmt);
+    $resultData = mysqli_stmt_get_result($stmt);
+
+    $row = mysqli_fetch_assoc($resultData);
+    $productname = $row["product_name"];
+
+
+
+    return $productname;
+}
+function getUserID($conn,$username){
+
+    $sql = "SELECT id FROM site_user WHERE upper(user_uid)=upper(?) ;";
+    $stmt = mysqli_stmt_init($conn);
+
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: ../index.php?error=stmtfailed");
+        exit();
+    }
+    mysqli_stmt_prepare($stmt, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $username);
+    mysqli_stmt_execute($stmt);
+    $resultData = mysqli_stmt_get_result($stmt);
+
+    $row = mysqli_fetch_assoc($resultData);
+    $id = $row["id"];
+
+
+
+
+    return $id;
+}
+
+function orderlineToTEXT($conn,$order){
+    $string ="";
+     for($k=0;$k<count($order);$k++){
+         $so= $order[$k]["qty"]."x ".getProductName($conn,$order[$k]["product_item_id"])." -> ".$order[$k]["price"]." Euro <br>";
+         $string=$string.$so;
+     }
+
+     return $string;
+}
--- GüntherExpress/orderedProducts_admin.php
+++ GüntherExpress/orderedProducts_admin.php
@@ -4,7 +4,7 @@
     require_once "includes/admin_functions_inc.php";
 ?>
 <head>
-    <link rel="stylesheet" href="../CSS/header.css">
+    <link rel="stylesheet" href="CSS/orderedProducts.css">
 
 </head>
 <body>
@@ -12,46 +12,95 @@
 
 <div class="orderedProductList">
     <form action="orderedProducts_admin.php" method="post">
-        <input type="text" name="search" placeholder="UserID eingeben">
+        <input type="text" name="search" placeholder="UserID/Username" required>
         <button type="submit">Suche</button>
     </form>
 </div>
 <?php
 $amount = 3;
     if(isset($_POST["search"])){
-        echo "ssss";
+
+        $userid=$_POST["search"];
+        if(strlen($userid)>3){
+            $userid=getUserID($conn,$userid);
+        }
+        $productArray=getAllOrderedProductsFromUserID($conn,$userid);
+        if($productArray!=null){
+            echo"<h1>Alle Besttelungen von ".getUsername($conn,$productArray[0]["siteuser_id"])."</h1>
+                <table>
+                    <tr>
+                        <th>Order_id </th>
+                        <th>Username </th>
+                        <th>Order Date </th>
+                        <th>Payment Method </th>
+                        <th>Shipping Adress</th>
+                        <th>Shipping Method</th>
+                        <th>Order Total </th>
+                        <th>Order Status </th>
+                        <th>Order Content</th>
+                    </tr>
+            
+                    ";
+                    for($i=0;$i<count($productArray);$i++) {
+                        $order=getOrderLine($conn,$productArray[$i]["order_id"]);
+                        echo "
+                        <tr>
+                                <td > ".$productArray[$i]["order_id"]." </td >
+                                <td > ".getUsername($conn,$productArray[$i]["siteuser_id"])." </td >
+                                <td > ".$productArray[$i]["order_date"]." </td >
+                                <td > ".getPaymentMethod($conn,$productArray[$i]["payment_method_id"])." </td >
+                                <td > ".getShippingAdress($conn,$productArray[$i]["shipping_address_id"])." </td >
+                                <td > ".getShippingMethod($conn,$productArray[$i]["shipping_method_id"])." </td >
+                                <td > ".$productArray[$i]["order_total"]."Euro </td >
+                                <td > ".getOrderStatus($conn,$productArray[$i]["order_status_id"])." </td >   
+                                <td > ".orderlineToTEXT($conn,$order)." </td >   
+                                        
+               
+                            </tr >";
+            }
+        }else{
+
+            echo "<h1>Unbekannte ID</h1>";
+        }
+
+
     }else{
         $productArray=getAllOrderedProducts($conn);
 
-    echo"
-        <table>
-            <tr>
-                <th>order_id </th>
-                <th>siteuser_id </th>
-                <th>order_date </th>
-                <th>payment_method_id </th>
-                <th>shipping_address_id </th>
-                <th>shipping_method_id </th>
-                <th>order_total </th>
-                <th>order_status_id </th>
-        
-            </tr>
-    
-            ";
-            for($i=0;$i<count($productArray);$i++) {
-                echo "
-                <tr>
-                        <td > ".$productArray[$i]["order_id"]." </td >
-                        <td > ".$productArray[$i]["siteuser_id"]." </td >
-                        <td > ".$productArray[$i]["order_date"]." </td >
-                        <td > ".$productArray[$i]["payment_method_id"]." </td >
-                        <td > ".$productArray[$i]["shipping_address_id"]." </td >
-                        <td > ".$productArray[$i]["shipping_method_id"]." </td >
-                        <td > ".$productArray[$i]["order_total"]." </td >
-                        <td > ".$productArray[$i]["order_status_id"]." </td >                    
-                    </tr >";
-            };
-        }
+        echo"
+                <h1>Alle Besttelungen</h1>
+                <table>
+                    <tr>
+                        <th>Order_id </th>
+                        <th>Username </th>
+                        <th>Order Date </th>
+                        <th>Payment Method </th>
+                        <th>Shipping Adress</th>
+                        <th>Shipping Method</th>
+                        <th>Order Total </th>
+                        <th>Order Status </th>
+                        <th>Order Content</th>
+                    </tr>
+            
+                    ";
+        for($i=0;$i<count($productArray);$i++) {
+            $order=getOrderLine($conn,$productArray[$i]["order_id"]);
+            echo "
+                        <tr>
+                                <td > ".$productArray[$i]["order_id"]." </td >
+                                <td > ".getUsername($conn,$productArray[$i]["siteuser_id"])." </td >
+                                <td > ".$productArray[$i]["order_date"]." </td >
+                                <td > ".getPaymentMethod($conn,$productArray[$i]["payment_method_id"])." </td >
+                                <td > ".getShippingAdress($conn,$productArray[$i]["shipping_address_id"])." </td >
+                                <td > ".getShippingMethod($conn,$productArray[$i]["shipping_method_id"])." </td >
+                                <td > ".$productArray[$i]["order_total"]."Euro </td >
+                                <td > ".getOrderStatus($conn,$productArray[$i]["order_status_id"])." </td >   
+                                <td > ".orderlineToTEXT($conn,$order)." </td >   
+                                        
+               
+                            </tr >";
+            }
+    }
     
     ?>
 
